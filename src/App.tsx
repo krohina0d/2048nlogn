508eff8545956826c8e0aa590281ea1aa890f64b
spi-u <m2111815@edu.misis.ru>
2024-12-01 12:59:16 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import Game2048 from './components/Game2048';
 const App = () => {
   return (
     <Routes>
-      <Route path="/2048" element={<Game2048 />} />
-      <Route path="/" element={<Navigate to="/2048" replace />} />
+      <Route path="/" element={<Game2048 />} />
     </Routes>
   );
 };
